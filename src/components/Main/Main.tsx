9a336e66ed012d1938a8a3ae37c133b3a5273d0b
Ian Phua <ianphua3@gmail.com>
2021-08-19 17:26:43 -0400

    Completed app

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -19,7 +19,7 @@ const Main: React.FC = () => {
                 <Typography variant='subtitle1' style={{ lineHeight: '1.5em', marginTop: '20px' }}>
                     <InfoCard />
                 </Typography>
-                <Divider />
+                <Divider className={classes.divider} />
                 <Form />
             </CardContent>
             <CardContent className={classes.cartContent}>
